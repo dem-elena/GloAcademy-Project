3c1758e66acc51c3b4cd44aaad37e604e6f17087
Elena Demyashina <dem-ea@yandex.ru>
2022-01-28 22:48:31 +0300

    Добавила код к уроку 2(обяз)

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
-alert("HELLO, WORLD!!!");
+let title = "GloAcademy-Project";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 500;
+let rollback = 23;
+let fullPrice = 100000;
+let adaptive = "true";
+// alert("HELLO, WORLD!!!");
 console.log("Hello, all!");
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
+console.log(screens.length);
+console.log(
+  `Стоимость верстки экранов ${screenPrice} рублей/${screenPrice / 80} долларов`
+);
+console.log(screens.toLowerCase());
+console.log(screens.toLowerCase().split(","));
+console.log(
+  `Процент отката посреднику за работу ${fullPrice * (rollback / 100)}`
+);
