243127c151505a7dd467dceba141bb8e2ba3513a
Elena Demyashina <dem-ea@yandex.ru>
2022-01-30 18:24:25 +0300

    исправила недочеты 2 урока

--- js/script.js
+++ js/script.js
@@ -3,18 +3,21 @@ let screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 500;
 let rollback = 23;
 let fullPrice = 100000;
-let adaptive = "true";
+let adaptive = true;
 // alert("HELLO, WORLD!!!");
-console.log("Hello, all!");
-console.log(title);
-console.log(fullPrice);
-console.log(adaptive);
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 console.log(screens.length);
 console.log(
   `Стоимость верстки экранов ${screenPrice} рублей/${screenPrice / 80} долларов`
 );
+console.log(
+  `Стоимость разработки сайта ${fullPrice} рублей/${fullPrice / 80} долларов`
+);
 console.log(screens.toLowerCase());
-console.log(screens.toLowerCase().split(","));
+console.log(screens.toLowerCase().split(", "));
 console.log(
   `Процент отката посреднику за работу ${fullPrice * (rollback / 100)}`
 );
